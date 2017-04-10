dfc37d265e005d451a1687265ab8248c1db8525e
github username <github email address>
2017-04-10 11:39:01 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render 'index.html'
+  end
+  def show
+    #will refer to students/:id implicitly defined as show
+    @student = Student.new
+    @student.first_name = "Daenerys"
+    @student.last_name = "Targaryen"
+    @student.save
+    render 'show.html'
   end
 end
